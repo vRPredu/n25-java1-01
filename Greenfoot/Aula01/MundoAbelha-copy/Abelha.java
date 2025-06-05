5218745a5c70740c2eb92ce04dcf711e97ebba3d
pedro <pedrobertocco59@gmail.com>
2025-06-04 21:52:12 -0300

    aula02

--- Greenfoot/Aula01/MundoAbelha-copy/Abelha.java
+++ Greenfoot/Aula01/MundoAbelha-copy/Abelha.java
@@ -1,10 +1,10 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Abelha here.
+ * Está é uma classe que representará uma Abelha.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Pedro 
+ * @version 2025-06-04
  */
 public class Abelha extends Actor
 {
@@ -17,10 +17,15 @@ public class Abelha extends Actor
         // Add your action code here.
         
         move(1);
-        if (Greenfoot.getKey()=="left") {
-            turn(-10);
+        if (Greenfoot.isKeyDown("left")) {
+            turn(-5);
             
             
         }
+        if (Greenfoot.isKeyDown("right")){
+            turn(5);
+            
+        }
+        
     }
 }
